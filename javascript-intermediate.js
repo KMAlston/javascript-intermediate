25c29ae0d18df9892586ba97c157d782ecc2af57
Krystal Alston <k.moore0203@gmail.com>
2022-03-31 19:23:02 -0400

    updated array

--- javascript-intermediate.js
+++ javascript-intermediate.js
@@ -1,3 +1,36 @@
 // Array and Object
-const awesomeClass = ['student1','student2','student3','teacher']
-console.log(awesomeClass[3])
+// const awesomeClass = ['student1','student2','student3','teacher']
+// console.log(awesomeClass[3])
+
+// const breakfast = "pancakes"
+// const lunch = "hot dog"
+// const dinner = "chicken"
+// const myFood = [breakfast,lunch,dinner]
+// console.log({myFood})
+
+// const carNames = ['Rhapsody','Renegade', 'Prince']
+// const carModel = ['Wrx', 'Mini Cooper','Accord']
+// const carFavorite = [carNames,carModel]
+// console.log({carFavorite})
+
+const krystalProfile = {
+    
+    address: {
+        street: ' South Lynnhaven'
+        city: 'Virginia Beach'
+        state: 'VA'
+    }
+    name: {
+        first: 'Krystal'
+        last: 'Alston'
+    }
+    phone: {
+        primary: '123-546-7890'
+        secondary: '098-765-4321'
+    }
+    favoriteMusic: ['hip hop','r&b','soca']
+
+
+
+}
+console.log(krystalProfile.name.first, krystalProfile.name.last)
